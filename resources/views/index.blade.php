abf8728a54bcf4054dccca69cfd54f1feb9de631
shavenking <shavenking@gmail.com>
2022-06-04 22:07:34 +0800

    Fix bug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -291,16 +291,16 @@
                         {
                             type: 'text',
                             text: `
-                        訂購人：${data['purchaser_name']}
-                        到貨日期：${data['arrival_date']}
-                        希望到貨時段：${data['arrival_time']}
-                        匯款期限：${data['last_money_transfer_date']} 晚上 12 點前，並留言給小編您的帳號後五碼，以方便我們對帳哦！
+訂購人：${data['purchaser_name']}
+到貨日期：${data['arrival_date']}
+希望到貨時段：${data['arrival_time']}
+匯款期限：${data['last_money_transfer_date']} 晚上 12 點前，並留言給小編您的帳號後五碼，以方便我們對帳哦！
 
-                        戶名：黃香梅
-                        銀行：台新銀行 敦南分行 812
-                        帳號：28881003432265
+戶名：黃香梅
+銀行：台新銀行 敦南分行 812
+帳號：28881003432265
 
-                        如有問題，請在下方留言，我們會儘速爲您服務～
+如有問題，請在下方留言，我們會儘速爲您服務～
                     `
                         }
                     ]).then(() => {
